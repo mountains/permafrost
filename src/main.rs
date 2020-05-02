1498b0541a352a78dbd3355ffa287fd4fda83c25
William Desportes <williamdes@wdes.fr>
2020-05-02 22:02:57 +0200

    feat: add swagger
    Ref: https://github.com/GREsau/okapi/tree/v0.4.0/examples/json-web-api

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #![recursion_limit="256"]
+
 #[macro_use]
 extern crate rocket;
+
+#[macro_use]
+extern crate rocket_okapi;
+
 extern crate serde;
 extern crate serde_json;
+use rocket_okapi::swagger_ui::*;
 pub mod cors;
 pub mod models;
 pub mod routes;
@@ -14,8 +20,15 @@ fn main() {
     rocket::ignite()
         //.manage(connection::connect())
         .attach(cors::CorsFairing)
-        .mount("/", routes![
+        .mount("/", routes_with_openapi![
             routes::index::index,
         ])
+        .mount(
+            "/dev/swagger-ui/",
+            make_swagger_ui(&SwaggerUIConfig {
+                url: Some("../../openapi.json".to_owned()),
+                urls: None,
+            }),
+        )
         .launch();
 }
--- src/routes/index.rs
+++ src/routes/index.rs
@@ -1,3 +1,4 @@
+#[openapi]
 #[get("/")]
 pub fn index() -> &'static str {
     "No data should be lost, ever."
