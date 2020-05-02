a00afd40b7142c7be62f016f63b4fd699ca2c2fe
William Desportes <williamdes@wdes.fr>
2020-05-02 20:29:24 +0200

    feat: add basic features for the server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#![recursion_limit="256"]
+#[macro_use]
+extern crate rocket;
+extern crate serde;
+extern crate serde_json;
+pub mod cors;
+pub mod models;
+pub mod routes;
+pub mod ressources;
+
 fn main() {
     println!("No data should be lost, ever.");
+    rocket::ignite()
+        //.manage(connection::connect())
+        .attach(cors::CorsFairing)
+        .mount("/", routes![
+            routes::index::index,
+        ])
+        .launch();
 }
