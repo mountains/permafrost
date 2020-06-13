7d4229ffecc464c2ab648f46143b4adcf6d7ee71
William Desportes <williamdes@wdes.fr>
2020-06-13 15:33:04 +0200

    feat: add an example value
    Ref: https://github.com/GREsau/okapi/issues/11

--- src/ressources/Branches.rs
+++ src/ressources/Branches.rs
@@ -4,10 +4,15 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, JsonSchema)]
 #[serde(rename_all = "camelCase")]
 pub struct Branch {
+    #[schemars(example = "example_name")]
     pub name: String,
     pub is_remote: bool,
     pub is_head: bool,
     pub upstream_name: Option<String>,
+}
+
+pub fn example_name() -> &'static str {
+    "master"
 }
 
 impl Branch {
