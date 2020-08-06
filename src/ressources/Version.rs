fddbcfdbb58c387d1bfc11d9567ffc9f63e681b7
William Desportes <williamdes@wdes.fr>
2020-08-06 12:55:18 +0200

    feat: add examples for Version ressource

--- src/ressources/Version.rs
+++ src/ressources/Version.rs
@@ -4,6 +4,16 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, JsonSchema)]
 #[serde(rename_all = "camelCase")]
 pub struct Version {
+    #[schemars(example = "example_code")]
     pub code: String,
+    #[schemars(example = "example_description")]
     pub description: String,
+}
+
+pub fn example_code() -> &'static str {
+    "v1"
+}
+
+pub fn example_description() -> &'static str {
+    "A cool name"
 }
